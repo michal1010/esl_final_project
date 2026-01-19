b40122cf74d7141994de07c2af03ab2cb7e79f0f
EksPC <eksp2r@gmail.com>
2026-01-19 13:49:20 +0100

    implemented test set partition

--- data_analytics/get_datasets.py
+++ data_analytics/get_datasets.py
@@ -5,14 +5,18 @@ def get_federated_datasets_random(num_clients=5):
     for i in range(num_clients):
         x = pd.read_csv(f'../federated_data_random/client_random_{i+1}.csv')
         datasets.append(x)
-    return datasets
+    
+    test_set = pd.read_csv(f'../federated_data_random/client_random_test.csv')
+    return datasets,test_set
 
 def get_federated_datasets_sensitive(num_clients=5):
     datasets = []
     for i in range(num_clients):
         x = pd.read_csv(f'../federated_data_sensitive/client_skewed_{i+1}.csv')
         datasets.append(x)
-    return datasets
+
+    test_set = pd.read_csv(f'../federated_data_sensitive/client_sensitive_test.csv')
+    return datasets, test_set
 
 def get_centralized_dataset():
     data = pd.read_csv('../data/centralized_dataset.csv')
