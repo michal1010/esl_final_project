07f6a96bd99e7d0a7f04e2e5443cd7c11b22fb16
Adithya <132263703+CynicalStoik@users.noreply.github.com>
2026-01-25 16:37:00 +0100

    Common Dataset for testing

--- fedarated_model/task.py
+++ fedarated_model/task.py
@@ -16,6 +16,8 @@ CLIENTS = [
     "client_random_5.csv",
 ]
 
+TEST = "client_random_test.csv"
+
 TARGET = "is_recid"
 UNIQUE_LABELS = [0, 1]
 FEATURES = [
@@ -33,7 +35,7 @@ CATEGORICAL_FEATURES = {
 EXPECTED_DUMMY_COLS = []
 for col, categories in CATEGORICAL_FEATURES.items():
     # drop_first=True, so skip the first category
-    for cat in categories[1:]:
+    for cat in categories:
         EXPECTED_DUMMY_COLS.append(f"{col}_{cat}")
 # Logistic Regression initialization
 def create_logreg_model():
@@ -61,25 +63,30 @@ def set_model_params(model, params: List[np.ndarray]):
 def load_data_by_cid(cid: int) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     df = pd.read_csv(os.path.join(DATA_PATH, CLIENTS[cid]))
     df = df[df[TARGET] != -1]  # remove invalid labels
-
+    df2 = pd.read_csv(os.path.join(DATA_PATH,TEST))
+    df2=df2[df2[TARGET]!= -1]
     # Split features and target
     X = df[NUMERIC_FEATURES + list(CATEGORICAL_FEATURES.keys())].copy()
+    X_test=df2[NUMERIC_FEATURES + list(CATEGORICAL_FEATURES.keys())].copy()
+    y_test=df2[TARGET].copy()
     y = df[TARGET].copy()
 
     # One-hot encode categorical features
-    X = pd.get_dummies(X, columns=CATEGORICAL_FEATURES.keys(), drop_first=True)
-
+    X = pd.get_dummies(X, columns=CATEGORICAL_FEATURES.keys(), drop_first=False)
+    X_test=pd.get_dummies(X_test, columns=CATEGORICAL_FEATURES.keys(), drop_first=False)
     # Ensure all dummy columns exist (add missing ones as 0)
     for col in EXPECTED_DUMMY_COLS:
         if col not in X.columns:
             X[col] = 0
-
+        if col not in X_test.columns:
+            X_test[col] = 0
     # Reorder columns to a fixed order
     X = X[NUMERIC_FEATURES + EXPECTED_DUMMY_COLS]
+    X_test=X_test[NUMERIC_FEATURES + EXPECTED_DUMMY_COLS]
 
     # Train/test split
-    split = int(0.8 * len(X))
-    X_train, X_test = X.iloc[:split].values, X.iloc[split:].values
-    y_train, y_test = y.iloc[:split].values, y.iloc[split:].values
+    # split = int(0.8 * len(X))
+    X_train = X
+    y_train = y
 
     return X_train, y_train, X_test, y_test
